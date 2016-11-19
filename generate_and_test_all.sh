68bf1bc77f69ddff322b0c29ff9c0f568ff3c966
Loris Friedel <loris.friedel@gmail.com>
2016-11-19 11:39:12 +0100

    Add models + test

--- generate_and_test_all.sh
+++ generate_and_test_all.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
+./generate_models.sh loris ../Documents/ml/loris_img_HOG > ./log/model_i_loris_gen.log
+./generate_models.sh thomas ../Documents/ml/thomas_img_HOG > ./log/model_i_thomas_gen.log
+./generate_models.sh others_but_thomas ../Documents/ml/others_but_thomas_img_HOG > ./log/model_i_others_but_thomas_gen.log
 
-./generate_models.sh loris ../Documents/ml/loris_img_HOG > model_i_loris_gen.log
-./generate_models.sh thomas ../Documents/ml/thomas_img_HOG > model_i_thomas_gen.log
-./generate_models.sh others_but_thomas ../Documents/ml/others_but_thomas_img_HOG > model_i_others_but_thomas_gen.log
-
-./test_models.sh loris ../Documents/ml/loris_img_HOG > test_model_loris_gen.log
-./test_models.sh thomas ../Documents/ml/thomas_img_HOG > test_model_thomas_gen.log
-./test_models.sh others_but_thomas ../Documents/ml/others_but_thomas_img_HOG > test_model_others_but_thomas_gen.log
+./test_models.sh loris ../Documents/ml/loris_img_HOG > ./log/test_model_loris_gen.log
+./test_models.sh thomas ../Documents/ml/thomas_img_HOG > ./log/test_model_thomas_gen.log
+./test_models.sh others_but_thomas ../Documents/ml/others_but_thomas_img_HOG > ./log/test_model_others_but_thomas_gen.log
